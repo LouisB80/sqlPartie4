e18c3d6273cca0a69ef96e4f55ae947cacc5955d
LouisB80 <louisbernardh@gmail.com>
2020-01-20 10:59:56 +0100

    Partie 4 SQL

---
+++ exercice1Partie4Sql/exercice1Partie4.sql
@@ -0,0 +1,2 @@
+USE webDevelopment;
+INSERT INTO languages('JavaScript, version5', 'PHP, version 5.2', 'PHP version 5.4', 'HTML, version 5.1', 'JavaScript, version 6', 'JavaScript, version 7', 'JavaScript, version 8', 'PHP, version 7');
---
+++ exercice2Partie4Sql/exercice2Partie4.sql
@@ -0,0 +1,2 @@
+USE webDevelopment;
+INSERT INTO frameworks('Symfony, version 2.8', 'Symfony version 3', 'Jquery, version 1.6', 'Jquery, version 2.10');
