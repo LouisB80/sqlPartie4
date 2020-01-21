21e1978158dba4ac2dd099ae6c713b6ac387ecdb
LouisB80 <louisbernardh@gmail.com>
2020-01-21 16:59:48 +0100

    Correction

--- exercice1Partie4Sql/exercice1Partie4.sql
+++ exercice1Partie4Sql/exercice1Partie4.sql
@@ -1,2 +1,10 @@
-USE webDevelopment;
-INSERT INTO languages('JavaScript, version5', 'PHP, version 5.2', 'PHP version 5.4', 'HTML, version 5.1', 'JavaScript, version 6', 'JavaScript, version 7', 'JavaScript, version 8', 'PHP, version 7');
+USE `webDevelopment`;
+TRUNCATE TABLE `languages`;
+INSERT INTO `languages`(`languages`, `version`) VALUES ('JavaScript', 'version 5'),
+('PHP', 'version 5.2'),
+('PHP', 'version 5.4'),
+('HTML', 'version 5.1'),
+('JavaScript', 'version 6'),
+('JavaScript', 'version 7'),
+('JavaScript', 'version 8'),
+('PHP', 'version 7');
--- exercice2Partie4Sql/exercice2Partie4.sql
+++ exercice2Partie4Sql/exercice2Partie4.sql
@@ -1,2 +1,5 @@
-USE webDevelopment;
-INSERT INTO frameworks('Symfony, version 2.8', 'Symfony version 3', 'Jquery, version 1.6', 'Jquery, version 2.10');
+USE `webDevelopment`;
+INSERT INTO `frameworks`(`framework`, `version`) VALUES ('Symfony', 'version 2.8'),
+('Symfony', 'version 3'),
+('Jquery', 'version 1.6'),
+('Jquery', 'version 2.10');
